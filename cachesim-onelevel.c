10ca89f690c1303487e23bb680fae621f9b461cd
Hyunguk Ryu <hw0603@naver.com>
2022-07-25 02:16:14 +0900

    Create cachesim-onelevel.c

---
+++ cachesim-onelevel.c
@@ -0,0 +1,11 @@
+// file: cachesim-onelevel.c
+// author : Ryu Hyung Uk
+// description : Program to simulate one level cache
+// usage: ./cachesim -s=<cache size> -a=<set size> -b=<block size> -f=<trace file name>
+
+#include <stdio.h>
+
+int main() {
+
+    return 0;
+}
